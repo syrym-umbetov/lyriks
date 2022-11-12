1c77c794ae4c1f18d3910a002078ab0e34f084b0
Syrym Umbetov <umbetov.syrym@gmail.com>
2022-11-12 18:58:50 +0600

    feat: fetching with redux toolkit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "897b864698mshd9566898364e02dp11e5e2jsn05a7bf77f30a"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
